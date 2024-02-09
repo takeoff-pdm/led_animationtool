f677a9ee4963350304e72c7071eab382c192ab42
diesdasnicolas <diesdasnicolas@gmail.com>
2024-02-09 15:29:33 +0100

    Start testing backend (30% complete)

--- test/main.js
+++ test/main.js
@@ -1,34 +1,73 @@
 import fetch from "node-fetch";
 
-// // add
-// let data = {name: "Sektion 1", start_led: 0, end_led: 119};
+// Section
+function addSection(data) {    
+    fetch("http://0.0.0.0:8000/api/add/section", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(data)
+    }).then(res => {
+        return res.json();
+    }).then(json => console.log("Request complete! response:", json));
+}
 
-// fetch("http://192.168.178.178:8000/api/add/section", {
-//   method: "POST",
-//   headers: {'Content-Type': 'application/json'}, 
-//   body: JSON.stringify(data)
-// }).then(res => {
-//   console.log("Request complete! response:", res);
-// });
+function updateSection(data) {
+    fetch("http://0.0.0.0:8000/api/update/section", {
+    method: "POST",
+    headers: {'Content-Type': 'application/json'}, 
+    body: JSON.stringify(data)
+    }).then(res => {
+    return res.json();
+    }).then(json => console.log("Request complete! response:", json));
+}
 
-// // remove
-// let data2 = {name: "Banana"};
+function removeSection(data) {
+    fetch("http://0.0.0.0:8000/api/remove/section", {
+    method: "POST",
+    headers: {'Content-Type': 'application/json'}, 
+    body: JSON.stringify(data)
+    }).then(res => {
+    return res.json();
+    }).then(json => console.log("Request complete! response:", json));
+}
 
-// fetch("http://192.168.178.178:8000/api/remove/section", {
-//   method: "POST",
-//   headers: {'Content-Type': 'application/json'}, 
-//   body: JSON.stringify(data2)
-// }).then(res => {
-//   console.log("Request complete! response:", res);
-// });
+// ColorSequence
+function addColorSequence(data) {    
+    fetch("http://0.0.0.0:8000/api/add/color_sequence", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(data)
+    }).then(res => {
+        return res.json();
+    }).then(json => console.log("Request complete! response:", json));
+}
 
-// update
-let data3 = {old_name: "Sektion 1", name: "Banana", start_led: 2, end_led: 112};
+function updateColorSequence(data) {
+    fetch("http://0.0.0.0:8000/api/update/color_sequence", {
+    method: "POST",
+    headers: {'Content-Type': 'application/json'}, 
+    body: JSON.stringify(data)
+    }).then(res => {
+    return res.json();
+    }).then(json => console.log("Request complete! response:", json));
+}
 
-fetch("http://192.168.178.178:8000/api/update/section", {
-  method: "POST",
-  headers: {'Content-Type': 'application/json'}, 
-  body: JSON.stringify(data3)
-}).then(res => {
-  console.log("Request complete! response:", res);
-});
+function removeColorSequence(data) {
+    fetch("http://0.0.0.0:8000/api/remove/color_sequence", {
+    method: "POST",
+    headers: {'Content-Type': 'application/json'}, 
+    body: JSON.stringify(data)
+    }).then(res => {
+    return res.json();
+    }).then(json => console.log("Request complete! response:", json));
+}
+
+// Section
+// addSection({name: "Sektion 1", start_led: 0, end_led: 119})
+// updateSection({id: 0, name: "Banana", start_led: 2, end_led: 112})
+// removeSection({id: 0})
+
+// Color Sequence
+addColorSequence({name: "WeißRotWeiß", description: "Viel weiß wenig rot", selection: 0, color_amount: 1})
+updateColorSequence({id: 0, name: "RotWeißRot", description: "Viel rot wenig weiß", selection: 2, color_amount: 2})
+removeColorSequence({id: 1})
