823b399943be0d575ea6bc9ee9ab813b5ee3f76b
diesdasnicolas <80157943+diesdasnicolas@users.noreply.github.com>
2024-02-11 20:59:59 +0100

    Add animation data

--- __init__.py
+++ __init__.py
@@ -3,3 +3,26 @@ PORT = '8000'
 
 CONFIG_FILE = 'config.json'
 DATABASE_FILE = './db.db'
+
+# Animations
+
+ANIMATION_DATA = [
+    {
+        'name': 'Flow',
+        'description': 'Flowing colors',
+        'variation': 0,
+        'direction': 0
+    },
+    {
+        'name': 'Shooter',
+        'description': 'Shooting colors',
+        'variation': 0,
+        'direction': 0
+    },
+    {
+        'name': 'Strobo',
+        'description': 'Strobo effect',
+        'variation': 0,
+        'direction': 0
+    }
+]
