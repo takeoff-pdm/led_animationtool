47fe4f3e99c654cf0298c6ee1a29a627b6925c60
Frederick Jahn <frederick.paul.jahn@gmail.com>
2024-02-15 13:21:02 +0100

    added api call to addcolorsequencebutton

--- src/components/colors/MenuBar/AddColorSequenceButton/AddColorSequenceButton.tsx
+++ src/components/colors/MenuBar/AddColorSequenceButton/AddColorSequenceButton.tsx
@@ -1,24 +1,35 @@
 import { Button } from "@/components/ui/button";
 import { useColorsContext } from "../../ColorsContext/ColorsContext";
 import { ColorSequence } from "@/api/types";
+import { addColorSequence, getColorSequences } from "@/api/api-calls";
 
 export const AddColorSequenceButton: React.FC = () => {
-  const { colorSequences, setColorSequences } = useColorsContext();
+  const { setColorSequences } = useColorsContext();
 
-  const addNewColorSequence = () => {
-    setColorSequences([
-      ...colorSequences,
-      {
-        color_amount: 0,
-        description: "add a description",
-        name: "New Color Sequence",
-        selection: 1,
-      } as ColorSequence,
-    ]);
+  const addNewColorSequence = async () => {
+    const newColorSequence: ColorSequence = {
+      color_amount: 0,
+      description: "add a description",
+      name: "New Color Sequence",
+      selection: 1,
+    } as ColorSequence;
+
+    const response = await addColorSequence(newColorSequence);
+    if (response.success) {
+      const sequences_data = await getColorSequences();
+      if (sequences_data.color_sequences) {
+        setColorSequences(sequences_data.color_sequences as ColorSequence[]);
+      }
+    }
   };
 
   return (
-    <Button onClick={addNewColorSequence} className="min-w-[200px]:w-full ">
+    <Button
+      onClick={() => {
+        addNewColorSequence();
+      }}
+      className="min-w-[200px]:w-full "
+    >
       Add Color Sequence
     </Button>
   );
