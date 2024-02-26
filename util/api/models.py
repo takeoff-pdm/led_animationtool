7937f5ef41db5da2cc637496feb4b326de5ede2b
diesdasnicolas <diesdasnicolas@gmail.com>
2024-02-26 12:14:15 +0100

    Add section specific animation values

--- util/api/models.py
+++ util/api/models.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 
+
 class Id(BaseModel):
     id: int
+
 
 class Section(BaseModel):
     id: int | None = None
@@ -9,12 +11,14 @@ class Section(BaseModel):
     start_led: int
     end_led: int
 
+
 class ColorSequence(BaseModel):
     id: int | None = None
     name: str
     description: str
     selection: int
     color_amount: int
+
 
 class Color(BaseModel):
     id: int | None = None
@@ -24,17 +28,27 @@ class Color(BaseModel):
     green: int
     blue: int
 
+
 class Animation(BaseModel):
     id: int
+    section_id: int | None = None
     name: str
     description: str
     variation: int
     direction: int
-    
+    offset: int
+
+
 class Animate(BaseModel):
     color_sequence_id: int
     animation_id: int
     section_id: int
+
+
+class SectionAnimation(BaseModel):
+    section_id: int
+    animation_id: int
+
 
 class Value(BaseModel):
     value: int
