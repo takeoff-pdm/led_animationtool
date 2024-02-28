1a7d1c0086eb0a13ae037090d81e0ef728ce2be3
diesdasnicolas <80157943+diesdasnicolas@users.noreply.github.com>
2024-02-28 19:43:04 +0100

    Update testing

--- test/main.js
+++ test/main.js
@@ -198,27 +198,27 @@ function updateBpm(data) {
 
 // Section
 // addSection({name: "Sektion 2", start_led: 0, end_led: 59})
-// updateSection({id: 0, name: "Banana", start_led: 0, end_led: 59})
+// updateSection({id: 0, name: "Banana", start_led: 0, end_led: 299})
 // removeSection({id: 0})
 
 // Section Animation
 // getSectionAnimations({id: 1})
 
 // Color Sequence
-// addColorSequence({name: "Marlene", description: "halt Marlene", selection: 0, color_amount: 1})
-// updateColorSequence({id: 2, name: "Random", description: "halt random", selection: 1, color_amount: 4})
+// addColorSequence({name: "iwas", description: "halt iwas", selection: 0, color_amount: 5})
+// updateColorSequence({id: 0, name: "Random", description: "halt random", selection: 1, color_amount: 5})
 // removeColorSequence({id: 1})
 
 // Color
-// addColor({ color_sequence_id: 2, red: 10, green: 120, blue: 255 })
+// addColor({ color_sequence_id: 2, red: 0, green: 0, blue: 255 })
 // removeColor({ id: 16 })
 // updateColor({ id: 1, color_sequence_id: 0, position: 1, red: 0, green: 0, blue: 255 })
 
 // Animation
-// updateAnimation({ id: 0, name: 'MonoColor', description: 'Monochromatic color switching (different sections possible)', variation: 1, direction: 0 })
+// updateAnimation({ id: 0, name: 'MonoColor', description: 'Monochromatic color switching (different sections possible)', variation: 1, direction: 0, section_id: 0 , offset: 0})
 // updateAnimation({ id: 1, name: 'Flow', description: 'Flow gliding through the pixels', variation: 0, direction: 0 })
-// startAnimation({color_sequence_id: 2, animation_id: 1, section_id: 0})
-// startAnimation({color_sequence_id: 0, animation_id: 0, section_id: 0})
+// startAnimation({color_sequence_id: 1, animation_id: 0, section_id: 0})
+// startAnimation({color_sequence_id: 1, animation_id: 0, section_id: 0})
 // stopAnimation({ id: 0 }) // Id is section_id
 
 // Settings
