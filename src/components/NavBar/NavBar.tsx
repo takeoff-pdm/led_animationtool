43ab750fd31a25bf2875ab4e8c21275ce5080b8b
Frederick Jahn <frederick.paul.jahn@gmail.com>
2024-03-14 12:43:42 +0100

    added hamburger nav menu on mobile

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,19 @@
 "use client";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { usePathname } from "next/navigation";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
 type NavBarItem = {
   display_name: string | React.ReactNode;
@@ -44,10 +55,53 @@ const ITEMS_NAVBAR: NavBarItem[] = [
 
 export const NavBar: React.FC = () => {
   return (
-    <div className="h-20 flex items-center sm:px-10 px-4  border-b">
-      {ITEMS_NAVBAR.map((item) => (
-        <NavBarItem key={item.href} {...item} />
-      ))}
+    <>
+      <div className="hidden h-20 items-center border-b px-4 sm:flex  md:px-10">
+        {ITEMS_NAVBAR.map((item) => (
+          <NavBarItem key={item.href} {...item} />
+        ))}
+      </div>
+      <MobileNavbar />
+    </>
+  );
+};
+
+const MobileNavbar: React.FC = () => {
+  // left side current page
+  // right side hamburger menu
+  const pathname = usePathname();
+
+  return (
+    <div className="flex h-20 w-full items-center justify-between border-b px-4 sm:hidden">
+      <div className="flex-1">
+        {ITEMS_NAVBAR.map((item) => (
+          <div
+            className={`${pathname.startsWith(item.href) ? "flex" : "hidden"}`}
+          >
+            <NavBarItem key={item.href} {...item} />
+          </div>
+        ))}
+      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size={"icon"} variant="outline">
+            <HamburgerMenuIcon className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>Led Manager</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            {ITEMS_NAVBAR.map((item) => (
+              <Link href={item.href}>
+                <DropdownMenuItem key={item.href}>
+                  {item.display_name}
+                </DropdownMenuItem>
+              </Link>
+            ))}
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
