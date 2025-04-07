110e32eca8f3801e8521c8f3882cea45b89526c8
Nicolas <n.goermer@icloud.com>
2025-04-07 19:47:15 +0200

    Update strobe animation

--- animations/strobe.py
+++ animations/strobe.py
@@ -1,81 +1,131 @@
 from time import sleep, time_ns
 from rpi_ws281x import Color as StripColor
 
+from __init__ import ANIMATION_STEPS
 from animations.animation import Animation
 
 
 class Strobe(Animation):
+    def select_range(self, index: int, colors: list) -> list:
+        animation_range = []
+
+        match self.direction:
+            case 0:  # Normal
+                animation_range.append(range(int(self.start_led + 1 / len(colors)
+                                                 * (self.end_led - self.start_led)
+                                                 * index),
+                                             int(self.end_led + 1
+                                                 - 1 / len(colors)
+                                                 * (self.end_led - self.start_led)
+                                                 * (len(colors) - index - 1))))
+
+            case 1:  # Reverse
+                animation_range.append(range(int(self.start_led + 1 / len(colors)
+                                                 * (self.end_led - self.start_led)
+                                                 * (len(colors) - index - 1)),
+                                             int(self.end_led + 1
+                                                 - 1 / len(colors)
+                                                 * (self.end_led - self.start_led)
+                                                 * index)))
+
+            case 2:  # Innter to outer
+                animation_range.append(range(int(self.start_led + 1 / len(colors)
+                                                 * ((self.end_led - self.start_led) / 2)
+                                                 * index),
+                                             int(self.end_led + 1
+                                                 - 1 / len(colors)
+                                                 * ((self.end_led - self.start_led) / 2)
+                                                 * (len(colors) - index - 1)
+                                                 - (self.end_led - self.start_led) / 2)))
+
+                animation_range.append(range(int(self.start_led
+                                                 + 1 / len(colors)
+                                                 * ((self.end_led - self.start_led) / 2)
+                                                 * (len(colors) - index - 1)
+                                                 + (self.end_led - self.start_led) / 2),
+                                             int(self.end_led + 1
+                                                 - 1 / len(colors)
+                                                 * ((self.end_led - self.start_led) / 2)
+                                                 * index)))
+
+            case 3:  # Outer to inner
+                animation_range.append(range(int(self.start_led + 1 / len(colors)
+                                                 * ((self.end_led - self.start_led) / 2)
+                                                 * (len(colors) - index - 1)),
+                                             int(self.end_led + 1
+                                                 - 1 / len(colors)
+                                                 * ((self.end_led - self.start_led) / 2)
+                                                 * index
+                                                 - (self.end_led - self.start_led) / 2)))
+
+                animation_range.append(range(int(self.start_led
+                                                 + 1 / len(colors)
+                                                 * ((self.end_led - self.start_led) / 2)
+                                                 * index
+                                                 + (self.end_led - self.start_led) / 2),
+                                             int(self.end_led + 1
+                                                 - 1 / len(colors)
+                                                 * ((self.end_led - self.start_led) / 2)
+                                                 * (len(colors) - index - 1))))
+
+        return animation_range
+    
     def animate(self, beat: int, step: int):
-        # NEEDS COMPLETE RENEWAL!!!!
-        # (Reformat old code by using step to determine the current state of the animation)
-        pass
+        colors = self.select_colors(beat)
 
-        # starting_time = time_ns() // 1_000_000
-        # print('Flacka flacka flacka')
+        # Direction is used to determine how often a flash occurs
+        if self.direction == 0 or (self.direction == 1 and beat % 2 == 0) or \
+           (self.direction == 2 and beat % 4 == 0) or (self.direction == 3 and beat % 8 == 0):
+            if self.variation == 1:  # Flash on the beat and turn off after 1/3 of the beat
+                if step == 0:
+                    # Set the color of the strip to the beat color
+                    for index, color in enumerate(colors):
+                        animation_range = self.select_range(index, colors)
 
-        # # Select colors and therefore the amount of blinks
-        # colors = self.select_colors(beat)
+                        if len(animation_range) == 0:
+                            break
 
-        # animation_range = range(self.start_led, self.end_led + 1)
+                        for i in animation_range:
+                            for j in i:
+                                self.set_pixel_color(j, color)
+                
+                elif step == round(ANIMATION_STEPS * .33):
+                    # Turn off the strip
+                    for i in range(self.start_led, self.end_led + 1):
+                        self.set_pixel_color(i, StripColor(0, 0, 0))
+            
+            elif self.variation == 2:  # Flash twice per beat and each flash turns off after 1/6 of the beat
+                if step == 0 or step == round(ANIMATION_STEPS * .5):
+                    # Set the color of the strip to the beat color
+                    for index, color in enumerate(colors):
+                        animation_range = self.select_range(index, colors)
 
-        # if self.variation == 0:  # Do all the blinking shortly after each other
-        #     for color in colors:
-        #         for i in animation_range:
-        #             self.set_pixel_color(i, color)
+                        if len(animation_range) == 0:
+                            break
 
-        #         show_strip_request()
+                        for i in animation_range:
+                            for j in i:
+                                self.set_pixel_color(j, color)
+                
+                elif step == round(ANIMATION_STEPS * .16) or step == round(ANIMATION_STEPS * .66):
+                    # Turn off the strip
+                    for i in range(self.start_led, self.end_led + 1):
+                        self.set_pixel_color(i, StripColor(0, 0, 0))
 
-        #         sleep(self.sleep_time * (0.2 / len(colors))
-        #               - (time_ns() // 1_000_000 - starting_time) / 1_000)  # Wait until blink shuts off
+            elif self.variation == 3:  # Flash three times per beat and each flash turns off after 1/8 of the beat
+                if step == 0 or step == round(ANIMATION_STEPS * .33) or step == round(ANIMATION_STEPS * .66):
+                    # Set the color of the strip to the beat color
+                    for index, color in enumerate(colors):
+                        animation_range = self.select_range(index, colors)
 
-        #         starting_time = time_ns() // 1_000_000
+                        if len(animation_range) == 0:
+                            break
 
-        #         for i in animation_range:
-        #             self.set_pixel_color(i, StripColor(0, 0, 0))
+                        for i in animation_range:
+                            for j in i:
+                                self.set_pixel_color(j, color)
 
-        #         show_strip_request()
-
-        #         sleep(self.sleep_time * (0.3 / len(colors))
-        #               - (time_ns() // 1_000_000 - starting_time) / 1_000)  # Wait until it's time to blink again
-
-        # elif self.variation == 1:  # Do all the blinking equally distributed
-        #     for color in colors:
-        #         for i in animation_range:
-        #             self.set_pixel_color(i, color)
-
-        #         show_strip_request()
-
-        #         sleep(self.sleep_time / len(colors) * (1/3)
-        #               - (time_ns() // 1_000_000 - starting_time) / 1_000)
-
-        #         starting_time = time_ns() // 1_000_000
-
-        #         for i in animation_range:
-        #             self.set_pixel_color(i, StripColor(0, 0, 0))
-
-        #         show_strip_request()
-
-        #         sleep(self.sleep_time / len(colors) * (2/3)
-        #               - (time_ns() // 1_000_000 - starting_time) / 1_000)
-
-        # color = Color(255, 255, 255)
-        # speed = 4
-        # random_color = True
-
-        # for x in range(4):
-        #     if random_color:
-        #         color = wheel((randint(0, 256)) & 255)
-
-        #     for y in range(speed):
-        #         for i in range(strip.numPixels()):
-        #             if y % 2 == 0 and not (x == 0 and y == 2):
-        #                 strip.setPixelColor(i, color)
-
-        #             elif x == 0 and y == 1:
-        #                 strip.setPixelColor(i, color)
-
-        #             else:
-        #                 strip.setPixelColor(i, 0)
-
-        #         strip.show()
-        #         sleep(sleep_time / speed)
+                elif step == round(ANIMATION_STEPS * .125) or step == round(ANIMATION_STEPS * .455) or step == round(ANIMATION_STEPS * .875):
+                    # Turn off the strip
+                    for i in range(self.start_led, self.end_led + 1):
+                        self.set_pixel_color(i, StripColor(0, 0, 0))
--- doc/animations.md
+++ doc/animations.md
@@ -11,6 +11,26 @@
 1. 
 2. 
 
+## Strobe ⬜⬛⬜⬛
+**Description**: *Flashes the color on the beat*
+
+**Variations**:
+1. *Flash on the beat and turn off after 1/3 of the beat*
+2. *Flash twice per beat and each flash turns off after 1/6 of the beat*
+3. *Flash three times per beat and each flash turns off after 1/8 of the beat*
+
+**Directions** (Direction is used to determine how often a flash occurs):
+1. Every beat
+2. Every 2nd beat
+3. Every 4th beat
+4. Every 8th beat
+
+___
+___
+___
+___
+## Do not work due to maintenance
+
 ## Flow 🟣⏩🟠
 **Description**: *Colorsections flow from one side to the other*
 
